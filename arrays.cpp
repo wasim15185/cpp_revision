cc7336b0dc3f8ce026a3d1acc7a1e49f8ef35f1c
Wasim Akram Biswas <63144434+wasim15185@users.noreply.github.com>
2022-02-10 22:44:42 +0530

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+/*
+u have must know " stack is low memory " and "Heap is large memory"
+
+*/
+
+int main(int argc, char const *argv[])
+{
+    
+     int a[]={5,9,8,7,}; // this is stack memory
+      
+     int *b= new int[9] {5,9,7,3,1} ; // this is heap memory
+  
+     return 0;
+  
+  
+}
