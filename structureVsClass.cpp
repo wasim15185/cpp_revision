b5d595f6d7db8caa2ba4f6799ff4bbb471fef6a3
Wasim Akram Biswas <63144434+wasim15185@users.noreply.github.com>
2022-02-18 22:42:52 +0530

    created

---
+++ structureVsClass.cpp
@@ -0,0 +1,41 @@
+/**
+ * 
+ * In C++ , Structer and Class are same but 
+ *  Diffrence between Structer and Class
+ * 
+ * Structer : In Structer by default all variables and methods  are public .
+ * 
+ * Class : In Class by default all variables and methods  are private .
+ * 
+ */
+
+
+struct IamStructer
+{
+    void display(){
+
+    } ;
+};
+
+
+class IamClass{
+
+void display(){
+
+} ;
+
+} ;
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    IamStructer s ;
+
+    IamClass c;
+
+
+
+    return 0;
+}
